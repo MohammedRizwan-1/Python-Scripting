8cfffdc8763bebfdaad3d8af6e70945c04fe908e
MohammedRizwan-1 <mohammedrizwannn@gmail.com>
2022-11-08 18:34:13 +0000

    PyBank_Finacial_Analysis_Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,17 +8,48 @@ budget_csv = os.path.join('PyBank', 'Resources','budget_data.csv')
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    #Skip title row in csv file & Create Variables
+# Skip title row in csv file & Create Variables
     next(csvreader)
     prof_loss = []
     date = []
+    revenue_change = []
+    average_revenue_change = []
+    min_prof_loss_change = []
+    max_prof_loss_change = []
 
     # Loop to Calculate Total Revenue and Total Months
     for row in csvreader:
         prof_loss.append(float(row[1]))
         date.append(row[0])
 
+    #Loop to Calculate Revenue Change
+    for i in range(1,len(prof_loss)):
+
+        #Total Change in Revenue
+        revenue_change.append(prof_loss[i] - prof_loss[i-1])
+
+        #Average Change in Revenue
+        average_revenue_change = sum(revenue_change)/len(revenue_change)
     
+        #Greatest decrease in Profit and The Date
+        min_prof_loss_change = min(revenue_change)
+        min_prof_loss_change_date = str(date[revenue_change.index(min(revenue_change))+1])
+
+        #Greatest increase in Profit and The Date
+        max_prof_loss_change = max(revenue_change)
+        max_prof_loss_change_date = str(date[revenue_change.index(max(revenue_change))+1])
+
+    print("Financial Analysis")
+    print("-----------------------------------")
+    print("Total Months:", len(date))
+    print("Total Revenue: $", int((sum(prof_loss))))
+    print("Average Change: $", round((average_revenue_change),2))
+    print("Greatest Increase in Revenue :", max_prof_loss_change_date, "($",int(max_prof_loss_change),")")
+    print("Greatest Decrease in Revenue :", min_prof_loss_change_date, "($",int(min_prof_loss_change),")")
+    
+
+
+
 
 
 
