ba1f298de54cc6a5c016305ae05df7cf413d769e
MohammedRizwan-1 <mohammedrizwannn@gmail.com>
2022-11-08 17:30:58 +0000

    Loop-Calculating Total Revenue and Total Months

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,20 @@ budget_csv = os.path.join('PyBank', 'Resources','budget_data.csv')
 #Read CSV convert data into list of Dictionairies
 with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    #Skip title row in csv file & Create Variables
+    next(csvreader)
+    prof_loss = []
+    date = []
+
+    # Loop to Calculate Total Revenue and Total Months
+    for row in csvreader:
+        prof_loss.append(float(row[1]))
+        date.append(row[0])
+
     
+
+
+
 
     
